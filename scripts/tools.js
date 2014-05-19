6080902f2b195e65dc2a9949c9c3926c257eb2cc
Tanguy Hélesbeux <tanguy.helesbeux@gmail.com>
2014-05-19 17:25:07 +0200

    use ajax to send email

--- scripts/tools.js
+++ scripts/tools.js
@@ -15,3 +15,19 @@ $(function() {
     }
   });
 });
+
+
+$(document).ready(function(){
+    $("#subscribe").submit(function() {
+        //Do the AJAX post
+        $.post($("#subscribe").attr("action"), $("#subscribe").serialize(), function(data){
+            if (data == "OK") {
+              $("#success").show();
+            } else {
+              $("fail").show();
+            }
+        });
+        //Important. Stop the normal POST
+        return false;
+    });
+});
